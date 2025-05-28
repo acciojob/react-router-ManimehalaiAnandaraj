52346fd34a67dfd632dac4857059a3a66e15c120
manimehalaianandaraj <manimehalaiannakodi@gmail.com>
2025-05-28 13:37:20 +0530

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,23 @@
-
 import React from "react";
-import {Route,Router,Switch}from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
 import Navigation from './Navigation';
 import './../styles/App.css';
 
-const App = () => {
+function App() {
   return (
-   <Router>
-    <Navigation />
-    <Switch>
-      <Route path ="/" element ={<Home />} />
-      <Route path ="/about" element ={<About />} />
-    </Switch>
-    </Router>
-  )
+    <div>
+      <h1>React Navigation</h1>
+      <Router>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
